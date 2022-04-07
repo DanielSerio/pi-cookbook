e46c830062dc42b035713696fd024d5b58f65f71
DanielSerio <dserio.d@gmail.com>
2022-04-07 10:17:16 -0400

    added header menu links and button

--- components/navigation/HeaderMenu.tsx
+++ components/navigation/HeaderMenu.tsx
@@ -1,12 +1,12 @@
-import { Burger, Menu } from '@mantine/core';
+import { Burger, Divider, Menu, useMantineColorScheme } from '@mantine/core';
 import { useDisclosure } from '@mantine/hooks';
 import { NextLink } from '@mantine/next'
 import React from 'react';
-import { Home, Plus } from 'tabler-icons-react';
+import { Home, Moon, Palette, Plus, Sun } from 'tabler-icons-react';
 
 export function HeaderMenu() {
   const [opened, handlers] = useDisclosure(false)
-
+  const { colorScheme, toggleColorScheme } = useMantineColorScheme()
   return (
     <Menu 
       opened={opened} 
@@ -16,6 +16,14 @@ export function HeaderMenu() {
         <Menu.Label>Main Menu</Menu.Label>
         <Menu.Item icon={<Home size={10}/>} component={NextLink} href="/">Home</Menu.Item>
         <Menu.Item icon={<Plus size={10}/>} component={NextLink} href="/add-recipe">Add Recipe</Menu.Item>
+        <Divider />
+        <Menu.Label>Settings</Menu.Label>
+        <Menu.Item 
+          icon={colorScheme === 'light' ? <Moon /> : <Sun />} 
+          component='button' 
+          onClick={() => toggleColorScheme()}>
+            {colorScheme === 'light' ? 'Darkmode' : 'Lightmode' }
+          </Menu.Item>
       </Menu>
   );
 }
