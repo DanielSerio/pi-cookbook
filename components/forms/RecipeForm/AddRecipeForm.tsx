c157db9b2db8a43d66a882ea968119489d513f01
DanielSerio <dserio.d@gmail.com>
2022-04-07 17:34:58 -0400

    added general & weather inputs

--- components/forms/RecipeForm/AddRecipeForm.tsx
+++ components/forms/RecipeForm/AddRecipeForm.tsx
@@ -1,4 +1,5 @@
-import { Container, createStyles, SimpleGrid } from '@mantine/core';
+import { Container, createStyles, Radio, RadioGroup, SimpleGrid, Textarea, TextInput } from '@mantine/core';
+import { formList, useForm } from '@mantine/form';
 import React from 'react';
 import { Form, FormSection } from './Form';
 import { createFormStyles } from './styles';
@@ -8,6 +9,29 @@ const useFormStyles = createStyles(create)
 
 export function AddRecipeForm() {
   const { classes } = useFormStyles()
+  const form = useForm({
+    initialValues: {
+      name: '',
+      description: '',
+      img: null,
+      weather: null,
+      ingredients: formList([
+        {
+          name: '',
+          uom: '',
+          quantity: ''
+        }
+      ]),
+      steps: formList([
+        {
+          sortkey: '',
+          value: ''
+        }
+      ])
+    }
+  })
+
+
   return (
     <Form>
       <Container>
@@ -18,13 +42,18 @@ export function AddRecipeForm() {
           }
         ]}>
           <FormSection title="General">
-            <span>General</span>
+            <TextInput mt={'md'} label='Name' {...form.getInputProps('name')} required/>
+            <Textarea mt={'md'} label='Description' {...form.getInputProps('description')} required/>
           </FormSection>
           <FormSection title="Image">
             <span>Image</span>
+
           </FormSection>
           <FormSection className={classes.full} title="Weather">
-            <span>Weather</span>
+            <RadioGroup {...form.getInputProps('weather')}>
+              <Radio label="Cold" value='cold'/>
+              <Radio label="Warm" value='warm'/>
+            </RadioGroup>
           </FormSection>
           <FormSection className={classes.full} title="Ingredients">
             <span>Ingredients</span>
