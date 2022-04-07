f53f0c4c071537e6a965dd09b7754d9415657910
DanielSerio <dserio.d@gmail.com>
2022-04-07 09:52:48 -0400

    added mantine provider

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,8 +1,27 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { useHotkeys, useLocalStorage } from '@mantine/hooks'
+import { ColorScheme, ColorSchemeProvider, MantineProvider } from '@mantine/core'
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const [colorScheme, setColorScheme] = useLocalStorage<ColorScheme>({
+    key: 'mantine-color-scheme',
+    defaultValue: 'light',
+    getInitialValueInEffect: true,
+  })
+
+  const toggleColorScheme = (value?: ColorScheme) =>
+    setColorScheme(value || (colorScheme === 'dark' ? 'light' : 'dark'))
+
+  useHotkeys([['mod+J', () => toggleColorScheme()]])
+
+  return (
+    <ColorSchemeProvider colorScheme={colorScheme} toggleColorScheme={toggleColorScheme}>
+      <MantineProvider theme={{ primaryColor: 'orange', colorScheme }}>
+        <Component {...pageProps} />
+      </MantineProvider>
+    </ColorSchemeProvider>
+  )
 }
 
 export default MyApp
