5a5630419c226c56b21b604ccb0cf48a02f8f9fd
DanielSerio <dserio.d@gmail.com>
2022-04-08 10:55:23 -0400

    added basic footer layout

--- components/layout/AppFooter.tsx
+++ components/layout/AppFooter.tsx
@@ -1,21 +1,43 @@
-import { Box, createStyles, CSSObject, Footer, MantineTheme, Text } from '@mantine/core';
+import { Box, BoxProps, Container, ContainerProps, createStyles, CSSObject, Footer, MantineTheme, Text } from '@mantine/core';
 import React from 'react';
 
 function createFooterStyles(t: MantineTheme): Record<string, CSSObject> {
   return ({
     footer: {
-      color: t.colorScheme === 'light' ? t.colors.dark : t.colors.gray[0]
+      color: t.colorScheme === 'light' ? t.colors.dark : t.colors.gray[0],
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'flex-end'
     }
   })
 }
 
 const useFooterStyles = createStyles(createFooterStyles)
 
+interface FooterProps extends ContainerProps {
+  center?: true
+}
+
+function FooterBlock({ children, center, ...props }: FooterProps) {
+  return (
+    <Box m={0} p={0}>
+      <Container sx={{ textAlign: center ? 'center' : 'left' }} {...props}>
+        {children}
+      </Container>
+    </Box>
+  )
+}
+
 export function AppFooter() {
   const { classes } = useFooterStyles()
   return (
     <Footer height={300} className={classes.footer}>
-      <Text color={'inherit'}>Footer</Text>
+      <FooterBlock center>
+        <Text 
+          sx={{width: '100%' }} 
+          color={'dimmed'} 
+          component={'small'}>Created by Dan Serio</Text>
+      </FooterBlock>
     </Footer>
   )
 }
